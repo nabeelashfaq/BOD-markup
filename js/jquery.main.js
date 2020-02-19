5265a56363d1756d00fc73d876b85060d95bfed8
Nabeel Ashfaq <nabeel.ashfaq@arbisoft.com>
2020-02-19 18:40:43 +0500

    added checkout cart component

---
+++ js/jquery.main.js
@@ -0,0 +1,30 @@
+$('.quantity').each(function() {
+    var spinner = $(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity__btn--up'),
+        btnDown = spinner.find('.quantity__btn--down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+  
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+  });
